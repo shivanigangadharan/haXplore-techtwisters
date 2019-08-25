3ebba5fdc37319381d18f2a71dbbd18ea9bd285c
parulagg27 <parulagg27@gmail.com>
2019-08-25 12:20:29 +0530

    dataframe conversion issue fixed

--- digifin-portal/server/pdf_parser.py
+++ digifin-portal/server/pdf_parser.py
@@ -1,6 +1,7 @@
 from camelot import read_pdf
 import pandas as pd
 import sys
+import json
 
 file_path = sys.argv[1]
 password = sys.argv[2]
@@ -12,7 +13,12 @@ rows = read_pdf(
     flavor='lattice',
     strip_text=' .\n'
     )
+df = []
 
 for i in range(0, len(rows)):
 	rows_df = rows[i].df
-	print (rows_df)
+	print rows_df
+	df.append(rows_df)
+df = pd.concat(df, ignore_index=True)
+print (df)
+
